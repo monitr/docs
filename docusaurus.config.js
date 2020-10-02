fe6f2b9d3b9f37eae174b05a7857272b6d0b527b
Michiel De Mey <de.mey.michiel@gmail.com>
2020-10-02 19:00:10 +0200

    Initial commit

---
+++ docusaurus.config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  title: 'Monitr',
+  titleDelimiter: '-',
+  tagline: '',
+  url: 'https://docs.monitr.be',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  favicon: 'img/favicon.ico',
+  organizationName: 'monitr', // Usually your GitHub org/user name.
+  projectName: 'docs', // Usually your repo name.
+  themeConfig: {
+    sidebarCollapsible: false,
+    navbar: {
+      style: 'primary',
+      logo: {
+        alt: 'Monitr Logo',
+        src: 'img/logo.svg',
+      },
+    },
+    colorMode: {
+      defaultMode: 'light',
+      disableSwitch: true,
+      respectPrefersColorScheme: false,
+    }
+  },
+  plugins: [
+    ['@docusaurus/plugin-content-docs', {
+      routeBasePath: '/',
+      sidebarPath: require.resolve('./sidebars.js'),
+    }],
+    '@docusaurus/plugin-debug',
+    // '@docusaurus/plugin-google-analytics',
+    // '@docusaurus/plugin-google-gtag',
+    '@docusaurus/plugin-sitemap',
+    '@docusaurus/plugin-ideal-image'
+  ],
+  themes: [
+    ['@docusaurus/theme-classic', {
+      customCss: require.resolve('./src/css/custom.css'),
+    }],
+    // '@docusaurus/theme-search-algolia'
+  ],
+};
---
+++ sidebars.js
@@ -0,0 +1,18 @@
+module.exports = {
+  main: [
+    {
+      type: 'doc',
+      id: 'welcome',
+    },
+    {
+      type: 'category',
+      label: 'Implementation',
+      items: ['doc1', 'doc2', 'doc3'],
+    },
+    {
+      type: 'category',
+      label: 'Features',
+      items: ['mdx'],
+    }
+  ],
+};
